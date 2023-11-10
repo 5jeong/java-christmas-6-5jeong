69cf710a0f1f6b4862897f27149277862fde9095
5jeong <wjdgns4385@naver.com>
2023-11-11 02:42:59 +0900

    [Feat] 요일 enum 구현

---
+++ src/main/java/christmas/domain/DayOfWeek.java
@@ -0,0 +1,22 @@
+package christmas.domain;
+
+public enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY;
+
+    public boolean isWeekday() {
+        return this != FRIDAY && this != SATURDAY;
+    }
+
+    public boolean isWeekend() {
+        return this == FRIDAY || this == SATURDAY;
+    }
+    public boolean isSpecialDay(){
+        return this == SUNDAY;
+    }
+}
