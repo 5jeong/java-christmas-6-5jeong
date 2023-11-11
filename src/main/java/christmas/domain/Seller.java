b0846da3e1cfa6f2342f8285f7fb47c78e7a7d2a
5jeong <wjdgns4385@naver.com>
2023-11-12 03:39:05 +0900

    [Refactor] 불필요한 메세지 제거

--- src/main/java/christmas/domain/Seller.java
+++ src/main/java/christmas/domain/Seller.java
@@ -6,30 +6,27 @@ import java.util.Map;
 
 public class Seller {
 
+    private static final String ORDER_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private static Map<String, Integer> orderHistory;
 
     public Seller(String orderHistoty) {
-        validateDate(orderHistoty);
+        validateMenuAnoCount(orderHistoty);
         this.orderHistory = ConverterUtil.convertStringToMap(orderHistoty);
     }
 
-    public void validateDate(String orderHistoty) throws IllegalArgumentException {
-        try {
-            Map<String, Integer> orderResult = ConverterUtil.convertStringToMap(orderHistoty);
-            validateDuplicateMenu(orderHistoty, orderResult);
-            validateMenuExists(orderResult);
-            validateQuantity(orderResult);
-            validateDrinkOnly(orderResult);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+    public void validateMenuAnoCount(String orderHistoty) {
+        Map<String, Integer> orderResult = ConverterUtil.convertStringToMap(orderHistoty);
+        validateDuplicateMenu(orderHistoty, orderResult);
+        validateMenuExists(orderResult);
+        validateQuantity(orderResult);
+        validateDrinkOnly(orderResult);
     }
 
     private void validateMenuExists(Map<String, Integer> orderResult) {
         // 메뉴가 메뉴판에 없는 경우
         for (String orderMenu : orderResult.keySet()) {
             if (!MenuContains(orderMenu)) {
-                throw new IllegalArgumentException("메뉴판에업슴");
+                throw new IllegalArgumentException();
             }
         }
     }
@@ -37,7 +34,7 @@ public class Seller {
     private void validateDuplicateMenu(String orderHistoty, Map<String, Integer> orderMap) {
         List<String> orderHistotys = ConverterUtil.covertStringToList(orderHistoty);
         if (orderHistotys.size() != orderMap.size()) {
-            throw new IllegalArgumentException("중복");
+            throw new IllegalArgumentException();
         }
     }
 
@@ -49,14 +46,14 @@ public class Seller {
                 return;
             }
         }
-        throw new IllegalArgumentException("음료만 입력함");
+        throw new IllegalArgumentException();
     }
 
     private void validateQuantity(Map<String, Integer> orderResult) {
         // 메뉴의 개수가 1 이상 20 이하가 아닌 경우
         for (String orderMenu : orderResult.keySet()) {
             if (orderResult.get(orderMenu) < 1 || orderResult.get(orderMenu) > 20) {
-                throw new IllegalArgumentException("1이상 20이하개만 주문가능");
+                throw new IllegalArgumentException();
             }
         }
     }
@@ -69,20 +66,21 @@ public class Seller {
         }
         return false;
     }
-    public int totalOrderAmount(){
-        int totalOrderAmount=0;
-        for(String orderMenu : orderHistory.keySet()){
+
+    public int totalOrderAmount() {
+        int totalOrderAmount = 0;
+        for (String orderMenu : orderHistory.keySet()) {
             totalOrderAmount += Menu.valueOf(orderMenu).getPrice() * orderHistory.get(orderMenu);
         }
         return totalOrderAmount;
     }
 
-    public boolean isServiceMenu(){
+    public boolean isServiceMenu() {
         return totalOrderAmount() >= 120000;
     }
 
 
-    public  Map<String, Integer> getOrderHistory() {
+    public Map<String, Integer> getOrderHistory() {
         return orderHistory;
     }
 }
