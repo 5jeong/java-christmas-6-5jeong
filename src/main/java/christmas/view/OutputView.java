dd2c5a579fe0befe47e6726595bddc1416247fcc
5jeong <wjdgns4385@naver.com>
2023-11-12 03:38:10 +0900

    [Feat] 혜택내역, 총혜택 금액, 예상 결제금액, 배지 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,14 +5,20 @@ import java.util.Map;
 
 public class OutputView {
     private static final String ORDER_MENU = "%s %d개";
-    private static final String TOTAL_ORDER = "%s원";
-
+    private static final String AMOUNT_MESSAGE = "%s원";
+    private static final String CHRISTMAS_DISCOUNT = "크리스마스 디데이 할인: -%s원";
+    private static final String WEEKDAY_DISCOUNT = "평일 할인: -%s원";
+    private static final String WEEKEND_DISCOUNT = "주말 할인: -%s원";
+    private static final String SPECIAL_DISCOUNT = "특별 할인: -%s원";
+    private static final String SERVICE_BENEFIT = "증정이벤트: -%s원";
     private static final String EVENT_PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU_MESSAGE = "<주문 메뉴>";
     private static final String TOTAL_ORDER_AMOUNT = "<할인 전 총주문 금액>";
     private static final String SERVICE_MENU_MESSAGE = "<증정 메뉴>";
     private static final String BENEFIT_MESSAGE = "<혜택 내역>";
-    private static final String TOTAL_BENEFIT_MESSAGE_MESSAGE = "<총혜택 금액>";
+    private static final String TOTAL_BENEFIT_MESSAGE = "<총혜택 금액>";
+    private static final String TOTAL_BENEFIT = "-%s원";
+
     private static final String EXPECT_PAYMENT_AMOUNT_MESSAGE = "<할인 후 예상 결제 금액>";
     private static final String EVENT_GIFT_MESSAGE = "<12월 이벤트 배지>";
 
@@ -27,45 +33,85 @@ public class OutputView {
         System.out.println();
         System.out.println(ORDER_MENU_MESSAGE);
         for (String menu : orderMenu.keySet()) {
-            System.out.println(String.format(ORDER_MENU,menu,orderMenu.get(menu)));
+            System.out.println(String.format(ORDER_MENU, menu, orderMenu.get(menu)));
         }
     }
 
     public void totalOrderAmountOutput(int totalOrderAmount) {
         System.out.println();
         System.out.println(TOTAL_ORDER_AMOUNT);
-        System.out.println(String.format(TOTAL_ORDER,formatter.format(totalOrderAmount)));
+        System.out.println(String.format(AMOUNT_MESSAGE, formatter.format(totalOrderAmount)));
 
     }
 
     public void serviceMenuMessageOutput(boolean isServiceMenu) {
         System.out.println();
         System.out.println(SERVICE_MENU_MESSAGE);
-        if(isServiceMenu){
+        if (isServiceMenu) {
             System.out.println("샴페인 1개");
             return;
         }
         System.out.println("없음");
     }
 
-    public void benefitMessageOutput() {
+    public void benefitHistoryOutput(int totalBenefitAmount) {
         System.out.println();
         System.out.println(BENEFIT_MESSAGE);
+        if (totalBenefitAmount == 0) {
+            System.out.println("없음");
+        }
     }
 
-    public void totalBenefitMessageOutput() {
+    public void christmasDiscountOutput(int christmasDiscountAmount) {
+        if (christmasDiscountAmount != 0) {
+            System.out.println(String.format(CHRISTMAS_DISCOUNT, formatter.format(christmasDiscountAmount)));
+        }
+    }
+
+    public void weekdayDiscountOutput(int weekdayDiscountAmount) {
+        if (weekdayDiscountAmount != 0) {
+            System.out.println(String.format(WEEKDAY_DISCOUNT, formatter.format(weekdayDiscountAmount)));
+        }
+    }
+
+    public void weekendDiscountOutput(int weekendDiscountAmount) {
+        if (weekendDiscountAmount != 0) {
+            System.out.println(String.format(WEEKEND_DISCOUNT, formatter.format(weekendDiscountAmount)));
+        }
+    }
+
+    public void SpecialDiscountOutput(int specialDiscountAmount) {
+        if (specialDiscountAmount != 0) {
+            System.out.println(String.format(SPECIAL_DISCOUNT, formatter.format(specialDiscountAmount)));
+        }
+    }
+
+    public void serviceBenefitOutput(int serviceBenefitAmount) {
+        if (serviceBenefitAmount != 0) {
+            System.out.println(String.format(SERVICE_BENEFIT, formatter.format(serviceBenefitAmount)));
+        }
+    }
+
+    public void totalBenefitMessageOutput(int totalBenefitAmount) {
         System.out.println();
-        System.out.println(TOTAL_BENEFIT_MESSAGE_MESSAGE);
+        System.out.println(TOTAL_BENEFIT_MESSAGE);
+        if (totalBenefitAmount != 0) {
+            System.out.println(String.format("-" + AMOUNT_MESSAGE, formatter.format(totalBenefitAmount)));
+            return;
+        }
+        System.out.println(String.format(AMOUNT_MESSAGE, formatter.format(totalBenefitAmount)));
     }
 
-    public void expectPaymentAmountOutput() {
+    public void expectPaymentAmountOutput(int expectPaymentAmount) {
         System.out.println();
         System.out.println(EXPECT_PAYMENT_AMOUNT_MESSAGE);
+        System.out.println(String.format(AMOUNT_MESSAGE, formatter.format(expectPaymentAmount)));
     }
 
-    public void eventGiftMessageOutput() {
+    public void eventGiftMessageOutput(String present) {
         System.out.println();
         System.out.println(EVENT_GIFT_MESSAGE);
+        System.out.println(present);
     }
 
 
