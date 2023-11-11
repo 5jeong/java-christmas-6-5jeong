9172b37914fe15ccb85156c23387918c3e8f8672
5jeong <wjdgns4385@naver.com>
2023-11-12 03:37:41 +0900

    [Feat] 혜택내역, 총혜택 금액, 결제금액 배지 컨트롤러 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,6 +1,7 @@
 package christmas.controller;
 
 import christmas.domain.Buyer;
+import christmas.domain.PromotionEvent;
 import christmas.domain.Seller;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -15,6 +16,8 @@ public class ChristmasController {
     }
 
     public void christmasPromotion() {
+
+
         Buyer buyer = generateBuyer();
         Seller seller = generateSeller();
         previewBenefit(buyer,seller);
@@ -25,16 +28,48 @@ public class ChristmasController {
         outputView.orderMenuOutput(seller.getOrderHistory());
         outputView.totalOrderAmountOutput(seller.totalOrderAmount());
         outputView.serviceMenuMessageOutput(seller.isServiceMenu());
+        PromotionEvent promotionEvent = new PromotionEvent(buyer, seller);
+        int totalDiscoutAmount = promotionEvent.totalDiscountAmount();
+        int totalBenefitAmount = promotionEvent.totalBenefitAmount();
+        int expectPaymentAmount = seller.totalOrderAmount() - totalDiscoutAmount;
+        outputView.benefitHistoryOutput(totalBenefitAmount);
+        if(totalBenefitAmount!=0){
+            outputView.christmasDiscountOutput(promotionEvent.applyChristmasDiscount());
+            outputView.weekdayDiscountOutput(promotionEvent.applyWeekdayDiscount());
+            outputView.weekendDiscountOutput(promotionEvent.applyWeekendDiscount());
+            outputView.serviceBenefitOutput(promotionEvent.applySpecialDiscount());
+            outputView.serviceBenefitOutput(promotionEvent.applyServiceBenefit());
+        }
+        outputView.totalBenefitMessageOutput(totalBenefitAmount);
+        outputView.expectPaymentAmountOutput(expectPaymentAmount);
+        outputView.eventGiftMessageOutput(promotionEvent.SpecailPresent(totalBenefitAmount));
+
     }
 
 
     private Buyer generateBuyer(){
-        String visitDate = inputView.expectedVisitDateInput();
-        return new Buyer(visitDate);
+        while (true) {
+            try {
+                String visitDate = inputView.expectedVisitDateInput();
+                return new Buyer(visitDate);
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
+//        String visitDate = inputView.expectedVisitDateInput();
+//        return new Buyer(visitDate);
     }
 
     private Seller generateSeller(){
-        String orderMenuAndCount = inputView.orderMenuAndCountInput();
-        return new Seller(orderMenuAndCount);
+        while (true) {
+            try {
+                String orderMenuAndCount = inputView.orderMenuAndCountInput();
+                return new Seller(orderMenuAndCount);
+            } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
+//        String orderMenuAndCount = inputView.orderMenuAndCountInput();
+//        return new Seller(orderMenuAndCount);
     }
 }
