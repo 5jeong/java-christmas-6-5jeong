efdd014f934255e1f4c87ef1221ddfacfcd20ec9
5jeong <wjdgns4385@naver.com>
2023-11-14 00:18:21 +0900

    [Refactor] 메소드 분리, 공백제거, 사용하지않는 구문 제거

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -4,72 +4,108 @@ import christmas.domain.Buyer;
 import christmas.domain.PromotionEvent;
 import christmas.domain.Seller;
 import christmas.view.InputView;
+import christmas.view.OutputConstants;
 import christmas.view.OutputView;
 
 public class ChristmasController {
     private final InputView inputView;
     private final OutputView outputView;
 
-    public ChristmasController(){
-        this.inputView= new InputView();
+    public ChristmasController() {
+        this.inputView = new InputView();
         this.outputView = new OutputView();
     }
 
     public void christmasPromotion() {
-
-
         Buyer buyer = generateBuyer();
         Seller seller = generateSeller();
-        previewBenefit(buyer,seller);
-
+        confirmOrderStep(buyer, seller);
+        PromotionEvent promotionEvent = generatePromotionEvent(buyer, seller);
+        applyDiscountStep(promotionEvent);
     }
 
-    private void previewBenefit(Buyer buyer, Seller seller) {
-        outputView.orderMenuOutput(seller.getOrderHistory());
-        outputView.totalOrderAmountOutput(seller.totalOrderAmount());
-        outputView.serviceMenuMessageOutput(seller.isServiceMenu());
-        PromotionEvent promotionEvent = new PromotionEvent(buyer, seller);
-        int totalDiscoutAmount = promotionEvent.totalDiscountAmount();
-        int totalBenefitAmount = promotionEvent.totalBenefitAmount();
-        int expectPaymentAmount = seller.totalOrderAmount() - totalDiscoutAmount;
-        outputView.benefitHistoryOutput(totalBenefitAmount);
-        if(totalBenefitAmount!=0){
-            outputView.christmasDiscountOutput(promotionEvent.applyChristmasDiscount());
-            outputView.weekdayDiscountOutput(promotionEvent.applyWeekdayDiscount());
-            outputView.weekendDiscountOutput(promotionEvent.applyWeekendDiscount());
-            outputView.serviceBenefitOutput(promotionEvent.applySpecialDiscount());
-            outputView.serviceBenefitOutput(promotionEvent.applyServiceBenefit());
-        }
-        outputView.totalBenefitMessageOutput(totalBenefitAmount);
-        outputView.expectPaymentAmountOutput(expectPaymentAmount);
-        outputView.eventGiftMessageOutput(promotionEvent.SpecailPresent(totalBenefitAmount));
-
+    private void confirmOrderStep(Buyer buyer, Seller seller) {
+        displayEventPreview(buyer);
+        displayOrderMenu(seller);
+        displayTotalOrderAmount(seller);
     }
 
+    private void applyDiscountStep(PromotionEvent promotionEvent) {
+        displayServiceMenuMessage(promotionEvent);
+        displayBenefitHistory(promotionEvent);
+        displayBenefits(promotionEvent);
+        displayTotalBenefitMessage(promotionEvent);
+        displayExpectPaymentAmount(promotionEvent);
+        displayEventGiftMessage(promotionEvent);
+    }
 
-    private Buyer generateBuyer(){
+    private Buyer generateBuyer() {
+        outputView.startEventMessageOutput();
         while (true) {
             try {
                 String visitDate = inputView.expectedVisitDateInput();
                 return new Buyer(visitDate);
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                System.out.println(OutputConstants.VISIT_DATE_INPUT_ERROR_MESSAGE);
             }
         }
-//        String visitDate = inputView.expectedVisitDateInput();
-//        return new Buyer(visitDate);
     }
 
-    private Seller generateSeller(){
+    private Seller generateSeller() {
         while (true) {
             try {
                 String orderMenuAndCount = inputView.orderMenuAndCountInput();
                 return new Seller(orderMenuAndCount);
             } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
-                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                System.out.println(OutputConstants.ORDER_INPUT_ERROR_MESSAGE);
             }
         }
-//        String orderMenuAndCount = inputView.orderMenuAndCountInput();
-//        return new Seller(orderMenuAndCount);
+    }
+
+    private PromotionEvent generatePromotionEvent(Buyer buyer, Seller seller) {
+        return new PromotionEvent(buyer, seller);
+    }
+
+    private void displayEventPreview(Buyer buyer) {
+        outputView.eventPreviewMessageOutput(buyer.getVisitDay());
+    }
+
+    private void displayOrderMenu(Seller seller) {
+        outputView.orderMenuOutput(seller.getOrderHistory());
+    }
+
+    private void displayTotalOrderAmount(Seller seller) {
+        outputView.totalOrderAmountOutput(seller.totalOrderAmount());
+    }
+
+    private void displayServiceMenuMessage(PromotionEvent promotionEvent) {
+        outputView.serviceMenuMessageOutput(promotionEvent.isServiceMenu());
+    }
+
+    private void displayBenefitHistory(PromotionEvent promotionEvent) {
+        outputView.benefitHistoryOutput(promotionEvent.totalBenefitAmount());
+    }
+
+    private void displayBenefits(PromotionEvent promotionEvent) {
+        if (promotionEvent.totalBenefitAmount() != 0) {
+            outputView.christmasDiscountOutput(promotionEvent.applyChristmasDiscount());
+            outputView.weekdayDiscountOutput(promotionEvent.applyWeekdayDiscount());
+            outputView.weekendDiscountOutput(promotionEvent.applyWeekendDiscount());
+            outputView.specialDiscountOutput(promotionEvent.applySpecialDiscount());
+            outputView.serviceBenefitOutput(promotionEvent.applyServiceBenefit());
+        }
+    }
+
+    private void displayTotalBenefitMessage(PromotionEvent promotionEvent) {
+        outputView.totalBenefitMessageOutput(promotionEvent.totalBenefitAmount());
+    }
+
+    private void displayExpectPaymentAmount(PromotionEvent promotionEvent) {
+        int expectPaymentAmount = promotionEvent.expectPaymentAmount();
+        outputView.expectPaymentAmountOutput(expectPaymentAmount);
+    }
+
+    private void displayEventGiftMessage(PromotionEvent promotionEvent) {
+        outputView.eventGiftMessageOutput(promotionEvent.eventGift());
     }
 }
