a3cf7765fa52eb82ed20ba24254d2718b9c94e12
5jeong <wjdgns4385@naver.com>
2023-11-14 00:06:37 +0900

    [Feat] 입력검증 기능 추가 및 사용하지 않는 메소드 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,25 +1,23 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.util.ConverterUtil;
 
 public class InputView {
-    private static final String START_EVENT_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EXPECTED_VISIT_DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_MENU_AND_COUNT_INPUT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public void startEventMessageInput(){
-        System.out.println(START_EVENT_MESSAGE);
-    }
+    private final InputViewValidator inputViewValidator = new InputViewValidator();
+
     public String expectedVisitDateInput() {
         System.out.println(EXPECTED_VISIT_DATE_INPUT_MESSAGE);
         String expectedVisitDate = Console.readLine();
         return expectedVisitDate;
     }
 
-    public String orderMenuAndCountInput(){
+    public String orderMenuAndCountInput() {
         System.out.println(ORDER_MENU_AND_COUNT_INPUT_MESSAGE);
         String orderMenuAndCount = Console.readLine();
+        inputViewValidator.validateMenuFormat(orderMenuAndCount);
         return orderMenuAndCount;
     }
 
