6a36241568df9b4d8f6f45dc13a70d1e5602cff7
5jeong <wjdgns4385@naver.com>
2023-11-14 02:59:22 +0900

    [Refactor] 매개변수 수정

--- src/main/java/christmas/domain/SpecialDiscount.java
+++ src/main/java/christmas/domain/SpecialDiscount.java
@@ -2,18 +2,17 @@ package christmas.domain;
 
 public class SpecialDiscount {
     private static final int SPECIAL_DISCOUNT = 1000;
-    private static final int CHRISTMAS_DAY = 1000;
     private static final int NO_DISCOUNT_AMOUNT = 0;
 
-    public int calculateDiscount(int visitDay, DayOfWeek visitDayOfWeek) {
-        if (isSpecialEvent(visitDay, visitDayOfWeek)) {
+    public int calculateDiscount(int visitDay) {
+        if (isSpecialEvent(visitDay)) {
             return SPECIAL_DISCOUNT;
         }
         return NO_DISCOUNT_AMOUNT;
     }
 
-    private boolean isSpecialEvent(int visitDay, DayOfWeek visitDayOfWeek) {
-        return visitDay == CHRISTMAS_DAY || visitDayOfWeek.isSpecialDay();
+    private boolean isSpecialEvent(int visitDay) {
+        DecemberCalendar decemberCalendar = new DecemberCalendar();
+        return decemberCalendar.isSpecialDay(visitDay);
     }
-
 }
