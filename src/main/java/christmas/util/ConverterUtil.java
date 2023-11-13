b081d3b675f7428628d8f2ce8248093549363457
5jeong <wjdgns4385@naver.com>
2023-11-14 00:07:42 +0900

    [Refactor] 주석, 사용하지않는 구문 제거

--- src/main/java/christmas/util/ConverterUtil.java
+++ src/main/java/christmas/util/ConverterUtil.java
@@ -16,22 +16,14 @@ public class ConverterUtil {
                 .map(String::trim)
                 .map(item -> item.split("-"))
                 .collect(Collectors.toMap(
-                        parts -> parts[0],    // 음식의 이름
-                        parts -> Integer.parseInt(parts[1]),  // 수량
-                        (existing, replacement) -> existing + replacement, // 중복 메뉴 합치기
-                        LinkedHashMap::new // LinkedHashMap 사용하여 순서 보장
+                        parts -> parts[0],
+                        parts -> convertStringToInt(parts[1]),
+                        (existing, replacement) -> existing + replacement,
+                        LinkedHashMap::new
                 ));
     }
 
     public static Integer convertStringToInt(String str) {
         return Integer.parseInt(str);
     }
-
-    public static void main(String[] args) {
-        String input = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
-        Map<String, Integer> itemMap = convertStringToMap(input);
-        System.out.println(itemMap);
-
-    }
-
 }
