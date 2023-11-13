cf421a59f6e4eec5919493f9f6eec66164086c7c
5jeong <wjdgns4385@naver.com>
2023-11-14 00:03:41 +0900

    [Refactor] 변수명 수정, 사용하지 않는 변수 제거

--- src/main/java/christmas/domain/Buyer.java
+++ src/main/java/christmas/domain/Buyer.java
@@ -4,25 +4,23 @@ import christmas.util.ConverterUtil;
 import java.util.regex.Pattern;
 
 public class Buyer {
-    private final static String VISIT_DATE_INPUT_ERROR = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private static final String VISIT_DATE_REGEX = "[0-9]+";
     private static final int MIN_DATE = 1;
     private static final int MAX_DATE = 31;
 
-    private static int visitDay;
+    private static int visitDate;
 
     public Buyer(String visitDate) {
         validateDate(visitDate);
-        this.visitDay = ConverterUtil.convertStringToInt(visitDate);
+        this.visitDate = ConverterUtil.convertStringToInt(visitDate);
     }
 
     public DayOfWeek visitDayOfWeek() {
         DecemberCalendar decemberCalendar = new DecemberCalendar();
-        return decemberCalendar.getDayOfWeek(visitDay);
+        return decemberCalendar.getDayOfWeek(visitDate);
     }
 
     private void validateDate(String visitDate) {
-
         if (isNotDigit(visitDate)) {
             throw new IllegalArgumentException();
         }
@@ -41,6 +39,6 @@ public class Buyer {
     }
 
     public int getVisitDay() {
-        return visitDay;
+        return visitDate;
     }
 }
