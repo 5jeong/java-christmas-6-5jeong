b91c06fd041344fb414afb3c0a19c5d98639e687
5jeong <wjdgns4385@naver.com>
2023-11-14 22:35:02 +0900

    [Test] 평일할인 테스트코드 추가작성

--- src/test/java/christmas/domainTest/WeekdayDiscountTest.java
+++ src/test/java/christmas/domainTest/WeekdayDiscountTest.java
@@ -15,30 +15,49 @@ import org.junit.jupiter.api.Test;
 public class WeekdayDiscountTest {
 
     private WeekdayDiscount weekdayDiscount;
-    Map<String, Integer> orderHistory;
 
     @BeforeEach
     public void generateWeekdayDiscount() {
         weekdayDiscount = new WeekdayDiscount();
-        orderHistory = ConverterUtil.convertStringToMap("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
     }
 
     @DisplayName("평일 할인 적용 테스트")
     @Test
     void 평일_할인_적용_테스트() {
+        String orderMenu = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        Map<String, Integer> orderHistory = ConverterUtil.convertStringToMap(orderMenu);
         List<DayOfWeek> dayOfWeeks = new ArrayList<>(
                 List.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY,
                         DayOfWeek.SUNDAY));
+
         for (DayOfWeek dayOfWeek : dayOfWeeks) {
             int weekdayDiscountAmount = weekdayDiscount.calculateDiscount(dayOfWeek, orderHistory);
             assertThat(weekdayDiscountAmount).isEqualTo(4046);
         }
     }
 
-    @DisplayName("평일 할인 미적용 테스트")
+    @DisplayName("주말인경우, 평일 할인 미적용 테스트")
     @Test
-    void 평일_할인_미적용_테스트() {
+    void 주말일때_평일_할인_미적용_테스트() {
+        String orderMenu = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        Map<String, Integer> orderHistory = ConverterUtil.convertStringToMap(orderMenu);
         List<DayOfWeek> dayOfWeeks = new ArrayList<>(List.of(DayOfWeek.FRIDAY, DayOfWeek.SATURDAY));
+
+        for (DayOfWeek dayOfWeek : dayOfWeeks) {
+            int weekdayDiscountAmount = weekdayDiscount.calculateDiscount(dayOfWeek, orderHistory);
+            assertThat(weekdayDiscountAmount).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("평일이지만 디저트메뉴를 시키지않을때, 평일 할인 미적용 테스트")
+    @Test
+    void 평일일때_평일_할인_미적용_테스트() {
+        String orderMenu = "티본스테이크-1,바비큐립-1,제로콜라-1";
+        Map<String, Integer> orderHistory = ConverterUtil.convertStringToMap(orderMenu);
+        List<DayOfWeek> dayOfWeeks = new ArrayList<>(
+                List.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY,
+                        DayOfWeek.SUNDAY));
+
         for (DayOfWeek dayOfWeek : dayOfWeeks) {
             int weekdayDiscountAmount = weekdayDiscount.calculateDiscount(dayOfWeek, orderHistory);
             assertThat(weekdayDiscountAmount).isEqualTo(0);
