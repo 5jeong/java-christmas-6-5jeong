dd43e56b5f0e30c4778068c27124a56b0cfc3576
5jeong <wjdgns4385@naver.com>
2023-11-14 22:25:22 +0900

    [Test] 올바른 방문날짜 입력테스트 추가

--- src/test/java/christmas/domainTest/BuyerTest.java
+++ src/test/java/christmas/domainTest/BuyerTest.java
@@ -1,6 +1,7 @@
 package christmas.domainTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.domain.Buyer;
@@ -11,6 +12,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class BuyerTest {
+    @DisplayName("구매자의 방문날짜가 올바르게 입력한 경우 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12", "25"})
+    void 구매자의_방문날짜가_올바른경우_예외테스트(String vistiDate) {
+        assertThatCode(()-> new Buyer(vistiDate)).doesNotThrowAnyException();
+    }
 
     @DisplayName("구매자의 방문날짜가 숫자가 아니면 예외가 발생한다.")
     @ParameterizedTest
